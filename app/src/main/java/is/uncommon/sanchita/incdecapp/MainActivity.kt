83f6de4320e5fd71804d3df0a120626698d66510
sanchita <sanchita.ag@gmail.com>
2018-09-12 16:51:05 +0530

    Implement decrement function Add RxJava support

--- app/src/main/java/is/uncommon/sanchita/incdecapp/MainActivity.kt
+++ app/src/main/java/is/uncommon/sanchita/incdecapp/MainActivity.kt
@@ -4,21 +4,20 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.widget.Button
 import android.widget.TextView
+import com.jakewharton.rxbinding2.view.RxView
 
 class MainActivity : AppCompatActivity() {
 
-  private var currentDateValue: Int = 1
-  private var displayDate: Int = 0
-  private var unit: String = "days"
+  companion object {
+    const val DAYS = "days"
+    const val WEEKS = "weeks"
+    const val MONTHS = "months"
+  }
 
+  private var currentDateValue: Int = 0
   private lateinit var dateTextView: TextView
   private lateinit var incButton: Button
   private lateinit var decButton: Button
-
-
-  private val DAYS = "days"
-  private val WEEKS = "weeks"
-  private val MONTHS = "months"
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -28,55 +27,48 @@ class MainActivity : AppCompatActivity() {
     incButton = findViewById(R.id.plus)
     decButton = findViewById(R.id.minus)
 
-    incButton.setOnClickListener{
-      incrementDate()
-    }
-
-    decButton.setOnClickListener{
-      decrementDate()
-    }
+    val incrementStream = RxView.clicks(incButton)
+        .map { incrementDate() }
+    val decrementStream = RxView.clicks(decButton)
+        .map { decrementDate() }
+    incrementStream.mergeWith(decrementStream).subscribe()
 
   }
 
-  private fun incrementDate(){
-
-    if(currentDateValue / 7 == 0 || currentDateValue == 7){
-      unit = DAYS
-      displayDate = (currentDateValue)
-    } else if(currentDateValue / 7 == 1){
-      unit = WEEKS
-      displayDate = currentDateValue % 7 + 1
-    } else {
-      unit = MONTHS
-      displayDate = (currentDateValue - 14) + 2
-    }
+  private fun incrementDate() {
     currentDateValue++
+    val (displayDate, unit) = calculateDisplayDate()
     dateTextView.text = displayDate.toString() + unit
   }
 
-
-  private fun decrementDate(){
-
-    if(currentDateValue == 1){
+  private fun decrementDate() {
+    if (currentDateValue == 0) {
       dateTextView.text = "0days"
-      displayDate = 0
       return
     }
-    if(currentDateValue / 7 == 0 || currentDateValue == 7){
-      unit = DAYS
-      displayDate = (currentDateValue) % 7 - 2
-    } else if(currentDateValue / 7 == 1){
-      unit = WEEKS
-      displayDate = currentDateValue % 7 - 1
-    } else {
-      unit = MONTHS
-      displayDate = (currentDateValue - 14) + 2
-    }
     currentDateValue--
+    val (displayDate, unit) = calculateDisplayDate()
     dateTextView.text = displayDate.toString() + unit
-
-
-
   }
 
+  private fun calculateDisplayDate(): Pair<Int, String> {
+    var displayDate: Int
+    var unit: String
+
+    when {
+      currentDateValue / 7 == 0 || currentDateValue == 7 -> {
+        unit = DAYS
+        displayDate = currentDateValue
+      }
+      currentDateValue / 7 == 1 -> {
+        unit = WEEKS
+        displayDate = currentDateValue % 7 + 1
+      }
+      else -> {
+        unit = MONTHS
+        displayDate = (currentDateValue - 14) + 2
+      }
+    }
+    return Pair(displayDate, unit)
+  }
 }
